0d1dfd754227cbada2c11dc2308913da0e66a698
Clemens Bergmann <c.bergmann@proventa.de>
2015-11-10 16:03:16 +0100

    initial commit

---
+++ example.config.sh
@@ -0,0 +1,9 @@
+# config file for shellscript part of the zabbix->idoit integration
+#
+
+
+#idoit_url: the full url under idoit is reachable
+idoit_url="http://localhost/"
+
+#api_key: the API key for I-DoIT
+api_key=""
