1e2e4f78eeb34534f6c5665c72c688d1469eaf58
Clemens Bergmann <c.bergmann@proventa.de>
2015-11-10 16:29:47 +0100

    configure category and field IDs

--- example.config.sh
+++ example.config.sh
@@ -7,3 +7,13 @@ idoit_url="http://localhost/"
 
 #api_key: the API key for I-DoIT
 api_key=""
+
+#category ID for the Category with all active zabbix triggers
+zabbix_category="C__CATG__CUSTOM_FIELDS_ACTIVEZABBIXTRIGGERS"
+
+#field ID for trigger_id field
+trigger_id_name="f_text_c_1447146100601"
+#field ID for trigger_name field
+trigger_name_name="f_text_c_1447146011419"
+#field ID for trigger_date field
+trigger_date_name="f_text_c_1447146742919"
